9f9d91a0894d954679237f713f31a15e56d7cbbf
Mrfavour <tbonematlala@gmail.com>
2023-08-31 13:24:43 -0700

    why is it so important to dream

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _puts_recursion - function like puts();
+ * _puts_recursion - Prints a string followed by a new line;
  * @s: input
  * Return: Always 0 (Success)
  */
-void _puts_recursion(char *s) {
-  if (*s == '\0') {
-    putchar('\n');
-    return;
-  }
-
-  putchar(*s);
-  _puts_recursion(s+1);
+void _puts_recursion(char *s)
+{
+/* check for end of string */
+if (*s == '\0') 
+{
+/* print new line when end reached */
+putchar('\n');
+return;
+}
+putchar(*s);
+_puts_recursion(s+1);
 }
