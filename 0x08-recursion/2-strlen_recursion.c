22f66c79078f1e89d6ddcdef1c533e43d2f0736a
Mrfavour <tbonematlala@gmail.com>
2023-08-31 22:31:28 -0700

    Again

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,9 +6,11 @@
  *
  * Return: The length of the string
 */
-int _strlen_recursion(char *s) {
-if (*s == '\0') {
-return 0;
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return (0);
 }
-return 1 + _strlen_recursion(s + 1); 
+return (1 + _strlen_recursion(s + 1));
 }
