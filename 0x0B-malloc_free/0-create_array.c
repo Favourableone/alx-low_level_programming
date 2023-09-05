590d0cfe84fa63cd3dde39eaa3a1d1144be17c89
Favour <MrFavour>
2023-09-05 15:09:40 -0700

    again

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,16 +10,17 @@
 */
 char *create(unsigned int size, char c)
 {
+char *arr;
 if (size == 0)
 {return (NULL);
 }
-char *arr = (char *)malloc(size * sizeof(char));
+arr = malloc(size * sizeof(char));
 if (arr == NULL)
 {return (NULL);
 }
-for (unsigned int i = 0; i < size; i++)
-{
-arr[i] = c;
+unsigned int i;
+for (i = 0; i < size; i++)
+{arr[i] = c;
 }
 return (arr);
 }
