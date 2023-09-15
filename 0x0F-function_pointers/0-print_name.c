4148e2b52b18509432c9513c75c512f6192fec89
Favour <MrFavour>
2023-09-15 14:00:59 -0700

    whats my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,29 +2,11 @@
 #include "function_pointers.h"
 /**
  * print_name - Prints a name using a function pointer
- * @name: The name to print 
+ * @name: The name to print
  * @f: A function pointer to print the name
-*/
+ */
 void print_name(char *name, void (*f)(char *))
 {
+if (f != NULL)
 f(name);
-}
-/**
- * print_function - Prints a name 
- * @name: The name to print
-*/
-void print_function(char *name)
-{
-printf("Hello, my name is %s.\n", name); 
-}
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
-*/
-int main(void)
-{
-char *name = "Favour";
-print_name(name, print_function);
-return (0);
 }
