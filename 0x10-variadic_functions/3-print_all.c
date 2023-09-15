ef1c13b22b7a7f54db386d737b1e7b0c311242f0
Favour <MrFavour>
2023-09-15 15:36:47 -0700

    Again

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,51 +10,43 @@
 
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	int i = 0, j = 0;
-	char *str;
-	
-	va_start(args, format);
-	
-	while (format[i])
-	{
-		j = 0;
-		while (format[i] != 'c' &&
-		       format[i] != 'i' &&
-		       format[i] != 'f' &&
-		       format[i] != 's')
-			i++;
-		
-		switch (format[i])
-		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (!str)
-					printf("(nil)");
-				else
-					printf("%s", str);
-				break;
-			default:
-				j++;
-				break;				
-		}
-		
-		if (format[i + 1] && j < 2)
-			printf(", ");
-			
-		i++;
-	}
-	
-	printf("\n");
+	int i = 0;
+	char *str, *sep = "";
 
-	va_end(args);
+	va_list list;
+
+	va_start(list, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
+	}
+
+	printf("\n");
+	va_end(list);
 }
