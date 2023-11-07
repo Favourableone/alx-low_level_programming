df5456bce351d52024f96c56094a7a581ca8a2bb
Favour <favour@gmail.com>
2023-11-07 01:43:54 -0800

    again

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Prints the name of the file it was compiled from
+ * main - Prints the name of the file it was compiled from
  * Return: Always 0
- */
+ */
 int main(void)
 {
 printf("%s\n", __FILE__);
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,1 +1,6 @@
+#ifndef YOUR_HEADER_FILE_NAME_H
+#define YOUR_HEADER_FILE_NAME_H
+
 #define SUM(x, y) ((x) + (y))
+
+#endif
